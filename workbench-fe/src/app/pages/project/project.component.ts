741062584933b2ff32b412451c3989837655ea82
marselakhmetov <makhmetov@provectus.com>
2022-06-13 20:46:19 +0300

    Refactoring

--- workbench-fe/src/app/pages/project/project.component.ts
+++ workbench-fe/src/app/pages/project/project.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {Project, ProjectService, Roadmap} from "../../shared/services/client";
+import {Project, ProjectService, Roadmap, Task} from "../../shared/services/client";
 import {map, Observable, tap} from "rxjs";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 
@@ -32,11 +32,21 @@ export class ProjectComponent implements OnInit{
       this.project$ = this.projectService.getProjectById(this.id);
     }
     this.project$.pipe(
-        tap(p => this.roadmap = p.roadmap)
-    ).subscribe()
+        tap(p => this.roadmap = p.roadmap),
+  ).subscribe()
   }
 
   public openTask(taskId?: number): void {
     this.router.navigateByUrl('/project/' + this.id + '/task/' + taskId)
+  }
+
+  public compare(a: Task, b: Task) {
+      if ( a.status == 'LOCKED' ?  2 : a.status == 'TODO' ? 1 : 3 < (b.status == 'LOCKED' ?  2 : b.status == 'TODO' ? 1 : 3)){
+        return -1;
+      }
+      if ( a.status == 'LOCKED' ?  2 : a.status == 'TODO' ? 1 : 3 > (b.status == 'LOCKED' ?  2 : b.status == 'TODO' ? 1 : 3)){
+        return 1;
+      }
+      return 0;
   }
 }
